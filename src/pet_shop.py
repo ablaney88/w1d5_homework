291c83e200dd4f3b98c0b28e5af3ea2bb9d0a02e
ablaney88 <ablaney187@gmail.com>
2023-05-20 11:50:27 +0100

    another commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -5,12 +5,43 @@ def get_pet_shop_name(pet_shop):
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+    
+    
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
 
-
-
+def increase_pets_sold(pet_shop, sold):
+    pet_shop["admin"]["pets_sold"] += sold
+    
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+    
+    
+def get_pets_by_breed(pet_shop, breed):
+    found_pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            found_pets.append(pet)
+    return found_pets
 
 def find_pet_by_name(pet_shop, pet_name):
     for pet in pet_shop["pets"]:
         if pet["name"] == pet_name:
             return pet
+        
+def remove_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            pet_shop["pets"].remove(pet)
 
+def add_pet_to_stock(list_of_pets, pet):
+    list_of_pets["pets"].append(pet)
+    return len(list_of_pets["pets"])
+
+def get_customer_cash(customer_list):
+    return customer_list["cash"]
+
+def remove_customer_cash(customer_cash, amount):
+    customer_cash["cash"] -= amount
