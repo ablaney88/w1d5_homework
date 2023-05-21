cc35256cadc9c4440c2a2bb7d02aa78f485d4f2c
ablaney88 <ablaney187@gmail.com>
2023-05-21 17:08:52 +0100

    last commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -45,3 +45,57 @@ def get_customer_cash(customer_list):
 
 def remove_customer_cash(customer_cash, amount):
     customer_cash["cash"] -= amount
+
+def get_customer_pet_count(customers):
+    return len(customers["pets"])
+
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
+def customer_can_afford_pet(customers, new_pet):
+    customer_cash = customers["cash"]
+    pet_price = new_pet["price"]
+
+    if customer_cash >= pet_price:
+        return True
+    else:
+        return False
+    
+# def sell_pet_to_customer(pet_shop, pet, customer):
+    
+
+#     price_of_pet = pet["price"]
+
+#     # customer["pets"].append(pet)
+#     add_pet_to_customer(customer, pet)
+
+#     # pet_shop["admin"]["pets_sold"] += 1
+#     increase_pets_sold(pet_shop, 1)
+
+#     # customer["cash"] -= pet["price"]
+#     remove_customer_cash(customer, price_of_pet)
+
+#     # pet_shop["admin"]["total_cash"] += pet["price"]
+#     add_or_remove_cash(pet_shop, price_of_pet)
+
+
+def sell_pet_to_customer(pet_shop, pet, customer):        
+    if find_pet_by_name(pet_shop, pet) and customer_can_afford_pet(customer, pet):
+
+        price_of_pet = pet["price"]
+
+        customer["pets"].append(pet)
+        # add_pet_to_customer(customer, pet)
+
+        pet_shop["admin"]["pets_sold"] += len(customer["pets"])
+        # increase_pets_sold(pet_shop, get_customer_pet_count(customer))
+
+        # customer["cash"] -= pet["price"]
+        remove_customer_cash(customer, price_of_pet)
+
+        # pet_shop["admin"]["total_cash"] += pet["price"]
+        add_or_remove_cash(pet_shop, price_of_pet)
+
+    
+
+
